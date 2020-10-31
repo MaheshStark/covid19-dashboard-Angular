bc5893a54b8f0a5f1296b148842aba629fc3ff8f
MaheshStark <mbkmahesh1996@gmail.com>
2020-10-31 22:49:02 +0530

    initial commit with complete all basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Routes, RouterModule } from "@angular/router";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes =[
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path:'dashboard', component: DashboardComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) ,
+    HttpClientModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
